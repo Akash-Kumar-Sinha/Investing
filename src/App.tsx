79137657e5480318dd443c9ba8e84e873004de7c
Akash-KumarSinha <akashkumarsinha304@gmail.com>
2024-06-28 17:47:42 +0530

    revert shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "./components/ui/button";
+import Home from "./Pages/Home";
 
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-      <Button>Click me</Button>
-    </h1>
-  )
-}
+const App = () => {
+  return (<div>
+    <Home/>
+    </div>)
+};
+
+export default App;
